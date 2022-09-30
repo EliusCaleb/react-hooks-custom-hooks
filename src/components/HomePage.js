5b94f5ad09c0a72f277e04a75e7a786488848692
ELIUS <eliuswanyoro@gmail.com>
2022-09-30 15:09:54 +0300

    Done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,31 @@
 import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 
 function HomePage() {
-  // fetch data for posts
+  // fetch data for posts using custom hooks
+  const { data: posts, isLoaded } = useQuery("http://localhost:4000/posts");
+  // set the document title
+  useEffect(() => {
+    document.title = "Underreacted | Home";
+  }, []);
+  useDocumentTitle("Underreacted | Home");
+  return (
+    <>
+      <About />
+      {isLoaded ? <ArticleList posts={posts} /> : <h3>Loading...</h3>}
+    </>
+  );
+}
+
+export default HomePage;
+
+
+//code that is replace by hools 
+//const { posts, isLoaded } = useQuery();
+  /*
   const [isLoaded, setIsLoaded] = useState(false);
   const [posts, setPosts] = useState([]);
 
@@ -16,18 +38,4 @@ function HomePage() {
         setIsLoaded(true);
       });
   }, []);
-
-  // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
-
-  return (
-    <>
-      <About />
-      {isLoaded ? <ArticleList posts={posts} /> : <h3>Loading...</h3>}
-    </>
-  );
-}
-
-export default HomePage;
+  */
--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -1,9 +1,23 @@
 import { useEffect } from "react";
-
+/*
 function useDocumentTitle(pageTitle) {
   useEffect(() => {
     document.title = pageTitle;
   }, [pageTitle]);
 }
-
+*/
+// custom hooks for homepage
+/*
+function useDocumentTitle() {
+  useEffect(() => {
+    document.title = "Underreacted | Home";
+  }, []);
+}
+*/
+// custom hooks for arrticle page
+function useDocumentTitle(pageTitle) {
+  useEffect(() => {
+    document.title = pageTitle;
+  }, [pageTitle]);
+}
 export default useDocumentTitle;
